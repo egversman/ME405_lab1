2571698f08a0741b3a3cef1b77d2e3549d45c0c1
dave-reo <davidgiovannireo@gmail.com>
2023-01-26 14:25:38 -0800

    Update ME_405_Lab1.py

---
+++ ME_405_Lab1.py
@@ -0,0 +1,30 @@
+"""! @file main.py
+    This file controls a motor.
+"""
+
+import time
+
+def ch1_setup():
+    """!
+        
+    """
+    pinA0 = pyb.Pin (pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+    tim2 = pyb.Timer (0, 0xFFFF)
+    ch1 = tim2.channel (1, pyb.Timer, pin=pinA0)
+    return ch1
+
+def led_brightness(ch1, brightness_percent):
+    """! Control LED Brightness
+        @param  ch1 This is the channel that we want to control the brightness on
+        @param  brightness_percent This is a percent brightness for the LED   """ 
+    ch1.pulse_width_percent (brightness_percent)
+
+# The following code only runs if thus file is run as the main script;
+if __name__ == "__main__":
+    ch1 = led_setup()
+
+    while True:
+        for brightness_percent in range(101):
+            led_brightness(ch1, brightness_percent)
+            time.sleep(5/101)
+    
