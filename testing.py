49ab445b272bcd715680f6fef35b84e52fc71bf6
dave-reo <davidgiovannireo@gmail.com>
2023-01-28 16:04:58 -0800

    Saturday Attempt

--- testing.py
+++ testing.py
@@ -5,22 +5,27 @@
 # import motor_driver.py
 # import encoder_reader.py
 
-def chAB_setup(pin1, pin2):
+
+
+def chAB_setup():
     """!
         
     """
-    p1 = pyb.Pin(pyb.Pin.board.pin1, pyb.Pin.OUT_PP)
-    p2 = pyb.Pin(pyb.Pin.board.pin2, pyb.Pin.OUT_PP) 
+    pC6 = pyb.Pin("PC6", pyb.Pin.IN)
+    pC7 = pyb.Pin("PC7", pyb.Pin.IN)
     
-    tim = pyb.Timer(ENC_AB, prescaler = 0, period = 0xFFFF) 
+    tim = pyb.Timer(3, prescaler = 0, period = 0xFFFF) 
     
-    ch1 = tim.channel(1, pyb.Timer.PWM, pin=p1) 
-    ch2 = tim.channel(2, pyb.Timer.PWM, pin=p2)
+    ch1 = tim.channel(1, pyb.Timer.PWM, pin=pC6) 
+    ch2 = tim.channel(2, pyb.Timer.PWM, pin=pC7)
 
     return tim
 
 if __name__ == "__main__":
-    tim = chAB_setup(PB6, PB7)
+    while True:
+        tim = chAB_setup()
+        tim.counter()
+    # figure out what timer # works
     # Rotate motor by hand and use tim.counter() to verify ??
     
 
